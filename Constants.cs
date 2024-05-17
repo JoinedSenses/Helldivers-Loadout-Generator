6e706efdab643b7bd4e39fa1b6e85893f1d1af2d
JoinedSenses <JoinedSenses@gmail.com>
2024-05-16 20:38:30 -0400

    Init

---
+++ Constants.cs
@@ -0,0 +1,161 @@
+﻿namespace HelldiversLoadoutGenerator
+{
+    internal static class Constants
+    {
+        public static readonly string[] Primaries = {
+            "AR-23 Liberator (Assault)",
+            "AR-23P Liberator Penetrator (Assault)",
+            "AR-23C Liberator Concussive (Assault)",
+            "AR-61 Tenderizer (Assault)",
+            "BR-14 Adjudicator (Assault)",
+            "R-63 Diligence (Marksman)",
+            "R-63CS Diligence Counter Sniper (Marksman)",
+            "MP-98 Knight (Submachine)",
+            "SMG-37 Defender (Submachine)",
+            "SMG-72 Pummeler (Submachine)",
+            "SG-8 Punisher (Shotgun)",
+            "SG-8S Slugger (Shotgun)",
+            "SG-225 Breaker (Shotgun)",
+            "SG-225SP Breaker Spray&Pray (Shotgun)",
+            "SG-225IE Breaker Incendiary (Shotgun)",
+            "CB-9 Exploding Crossbow (Explosive)",
+            "JAR-5 Dominator (Explosive)",
+            "R-36 Eruptor (Explosive)",
+            "SG-8P Punisher Plasma (Energy)",
+            "ARC-12 Blitzer (Energy)",
+            "LAS-5 Scythe (Energy)",
+            "LAS-16 Sickle (Energy)",
+            "PLAS-1 Scorcher (Energy)",
+            "PLAS-101 Purifier (Energy)"
+        };
+
+        public static readonly string[] Secondaries = {
+            "P-2 Peacemaker (Pistol)",
+            "P-19 Redeemer (Pistol)",
+            "GP-31 Grenade Pistol (Pistol)",
+            "LAS-7 Dagger (Pistol)",
+            "P-113 Verdict (Pistol)",
+            "P-4 Senator (Pistol)"
+        };
+
+        public static readonly string[] Grenades = {
+            "G-6 Frag (Standard)",
+            "G-12 High Explosive (Standard)",
+            "G-10 Incendiary (Standard)",
+            "G-16 Impact (Special)",
+            "G-13 Incendiary Impact (Special)",
+            "G-23 Stun (Special)",
+            "G-3 Smoke (Special)",
+            "G-123 Thermite (Special)"
+        };
+
+        public static readonly string[] Armors = {
+            "FS-37 Ravager (Light, Engineering Kit)",
+            "CE-74 Breaker (Light, Engineering Kit)",
+            "CE-67 Titan (Light, Engineering Kit)",
+            "CM-21 Trench Paramedic (Light, Med-Kit)",
+            "CE-07 Demolition Specialist (Light, Engineering Kit)",
+            "FS-38 Eradicator (Light, Fortified)",
+            "EX-00 Prototype X (Light, Electrical Conduit)",
+            "SC-37 Legionnaire (Light, Servo-Assisted)",
+            "B-08 Light Gunner (Light, Extra Padding)",
+            "SC-30 Trailblazer Scout (Light, Scout)",
+            "SC-34 Infiltrator (Light, Scout)",
+            "SA-25 Steel Trooper (Medium, Servo-Assisted)",
+            "SA-12 Servo Assisted (Medium, Servo-Assisted)",
+            "CE-35 Trench Engineer (Medium, Engineering Kit)",
+            "CE-27 Ground Breaker (Medium, Engineering Kit)",
+            "CM-09 Bonesnapper (Medium, Med-Kit)",
+            "CM-10 Clinician (Medium, Med-Kit)",
+            "SC-15 Drone Master (Medium, Engineering Kit)",
+            "EX-03 Prototype 3 (Medium, Electrical Conduit)",
+            "EX-16 Prototype 16 (Medium, Electrical Conduit)",
+            "FS-34 Exterminator (Medium, Fortified)",
+            "DP-40 Hero Of The Federation (Medium, Democracy Protects)",
+            "DP-11 Champion Of The People (Medium, Democracy Protects)",
+            "CM-14 Physician (Medium, Med-Kit)",
+            "CE-81 Juggernaut (Medium, Engineering Kit)",
+            "DP-53 Savior Of The Free (Medium, Democracy Protects)",
+            "B-24 Enforcer (Medium, Fortified)",
+            "B-01 Tactical (Medium, Extra Padding)",
+            "CW-9 White Wolf (Medium, Extra Padding)",
+            "SA-04 Combat Technician (Medium, Scout)",
+            "FS-55 Devastator (Heavy, Fortified)",
+            "CW-22 Kodiak (Heavy, Fortified)",
+            "SA-32 Dynamo (Heavy, Servo-Assisted)",
+            "CM-17 Butcher (Heavy, Med-Kit)",
+            "FS-61 Dreadnought (Heavy, Servo-Assisted)",
+            "B-27 Fortified Commando (Heavy, Extra Padding)",
+            "FS-05 Marksman (Heavy, Fortified)",
+            "FS-11 Executioner (Heavy, Fortified)",
+            "CE-64 Grenadier (Heavy, Engineering Kit)",
+            "FS-23 Battle Master (Heavy, Fortified)",
+            "CW-36 Winter Warrior (Heavy, Servo-Assisted)"
+        };
+
+        public static readonly string[] Weapons = {
+            "Machine Gun (Weapon)",
+            "Anti-Materiel Rifle (Weapon)",
+            "Stalwart (Weapon)",
+            "Expendable Anti-Tank (Weapon)",
+            "Flamethrower (Weapon)",
+            "Heavy Machine Gun (Weapon)",
+            "Railgun (Weapon)",
+            "Grenade Launcher (Weapon)",
+            "Laser Cannon (Weapon)",
+            "Arc Thrower (Weapon)",
+            "Quasar Cannon (Weapon)"
+        };
+
+        public static readonly string[] WeaponsWithBackpack = {
+            "Recoilless Rifle (Weapon & Pack)",
+            "Autocannon (Weapon & Pack)",
+            "Airburst Rocket Launcher (Weapon & Pack)",
+            "Spear (Weapon & Pack)"
+        };
+
+        public static readonly string[] Backpacks = {
+            "Jump Pack (Backpack)",
+            "Supply Pack (Backpack)",
+            "Guard Dog Rover (Backpack)",
+            "Ballistic Shield (Backpack)",
+            "Shield Generator (Backpack)",
+            "Guard Dog (Backpack)"
+        };
+
+        public static readonly string[] Offensives = {
+            "Orbital Gatling Barrage (Offense)",
+            "Orbital Airburst Strike (Offense)",
+            "Orbital 120MM HE Barrage (Offense)",
+            "Orbital 380MM HE Barrage (Offense)",
+            "Orbital Laser (Offense)",
+            "Orbital Railcannon Strike (Offense)",
+            "Orbital Precision Strike (Offense)",
+            "Orbital Gas Strike (Offense)",
+            "Orbital EMS Strike (Offense)",
+            "Orbital Smoke Strike (Offense)",
+            "Eagle Strafing Run (Offense)",
+            "Eagle Airstrike (Offense)",
+            "Eagle Cluster Bomb (Offense)",
+            "Eagle Napalm Airstrike (Offense)",
+            "Eagle Smoke Strike (Offense)",
+            "Eagle 110MM Rocket Pods (Offense)",
+            "Eagle 500KG Bomb (Offense)"
+        };
+
+        public static readonly string[] Defensives = {
+            "HMG Emplacement (Defense)",
+            "Shield Generator Relay (Defense)",
+            "Tesla Tower (Defense)",
+            "Anti-Personnel Minefield (Defense)",
+            "Incendiary Mines (Defense)",
+            "Machine Gun Sentry (Defense)",
+            "Gatling Sentry (Defense)",
+            "Mortar Sentry (Defense)",
+            "Autocannon Sentry (Defense)",
+            "Rocket Sentry (Defense)",
+            "EMS Mortar Sentry (Defense)",
+            "Patriot Exosuit (Defense)"
+        };
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,274 @@
+﻿/*
+ * A console application which generates random loadouts for Helldivers 2
+ * Author: JoinedSenses
+ */
+namespace HelldiversLoadoutGenerator
+{
+    internal class Program
+    {
+        private static readonly Random Rand = new Random();
+
+        static void Main(string[] args)
+        {
+            Init();
+        }
+
+        private static void Init()
+        {
+            PrintHelp();
+
+            while (true)
+            {
+                Console.Write("Enter command: ");
+
+                string? input = Console.ReadLine();
+                if (input == null)
+                {
+                    continue;
+                }
+
+                input = input.Trim().ToLower();
+
+                if (input == "gen 4")
+                {
+                    Generate(4);
+                }
+                else if(input == "gen 3")
+                {
+                    Generate(3);
+                }
+                else if (input == "help")
+                {
+                    PrintHelp();
+                }
+                else if (input == "quit")
+                {
+                    Environment.Exit(0);
+                    break; // Not needed but VS complains
+                }
+                else
+                {
+                    Console.WriteLine(
+                        $"Unknown input: {input}\n"
+                    );
+                    break;
+                }
+            }
+        }
+
+        private static void PrintHelp()
+        {
+            Console.Clear();
+
+            Console.WriteLine(
+                "Helldivers Loadout Generator\n" +
+                "\n" +
+                "Commands:\n" +
+                "  gen 3 - Generate 3 stratagems\n" +
+                "  gen 4 - Generate 4 stratagems\n" +
+                "  help - List commands\n" +
+                "  quit - Close application\n\n"
+            );
+        }
+
+        private static void Generate(int stratagemCount)
+        {
+            Console.Clear();
+
+            GenerateCharacter();
+            GenerateStratagems(stratagemCount);
+        }
+
+        private static void GenerateCharacter()
+        {
+            int idx = Rand.Next(0, Constants.Armors.Length);
+            string armor = Constants.Armors[idx];
+
+            idx = Rand.Next(0, Constants.Primaries.Length);
+            string primary = Constants.Primaries[idx];
+
+            idx = Rand.Next(0, Constants.Secondaries.Length);
+            string secondary = Constants.Secondaries[idx];
+
+            idx = Rand.Next(0, Constants.Grenades.Length);
+            string grenade = Constants.Grenades[idx];
+
+            Console.WriteLine(
+                $"Arm:  {armor}\n" +
+                $"Pri:  {primary}\n" +
+                $"Sec:  {secondary}\n" +
+                $"Gre:  {grenade}\n"
+            );
+        }
+
+        private static void GenerateStratagems(int max)
+        {
+            bool selectedWeapon = false;
+            bool selectedBackpack = false;
+            bool selectedDefensive = false;
+
+            List<string> list = new();
+            list.AddRange(Constants.Weapons);
+            list.AddRange(Constants.WeaponsWithBackpack);
+            list.AddRange(Constants.Backpacks);
+            list.AddRange(Constants.Offensives);
+            list.AddRange(Constants.Defensives);
+
+            List<string> stratagems = new();
+
+            int i = 0;
+            while (i < max)
+            {
+                int idx = Rand.Next(0, list.Count);
+                var item = list[idx];
+
+                if (stratagems.Contains(item))
+                {
+                    continue;
+                }
+
+                if (IsWeapon(item, out bool hasPack))
+                {
+                    if (selectedWeapon)
+                    {
+                        continue;
+                    }
+
+                    selectedWeapon = true;
+
+                    if (!selectedBackpack && hasPack)
+                    {
+                        selectedBackpack = true;
+                    }
+
+                    if (i + 1 != max)
+                    {
+                        list = new();
+
+                        list.AddRange(Constants.Offensives);
+
+                        if (!selectedBackpack)
+                        {
+                            list.AddRange(Constants.Backpacks);
+                        }
+
+                        if (!selectedDefensive)
+                        {
+                            list.AddRange(Constants.Defensives);
+                        }
+                    }
+                }
+                else if (IsBackpack(item, out bool isWeapon))
+                {
+                    if (selectedBackpack)
+                    {
+                        continue;
+                    }
+
+                    selectedBackpack = true;
+
+                    if (!selectedWeapon && isWeapon)
+                    {
+                        selectedWeapon = true;
+                    }
+
+                    if (i + 1 != max)
+                    {
+                        list = new();
+
+                        list.AddRange(Constants.Offensives);
+
+                        if (!selectedWeapon)
+                        {
+                            list.AddRange(Constants.Weapons);
+                        }
+
+                        if (!selectedDefensive)
+                        {
+                            list.AddRange(Constants.Defensives);
+                        }
+                    }
+                }
+                else if (IsDefensive(item))
+                {
+                    if (selectedDefensive)
+                    {
+                        continue;
+                    }
+
+                    selectedDefensive = true;
+
+                    if (i + 1 != max)
+                    {
+                        list = new();
+
+                        list.AddRange(Constants.Offensives);
+
+                        if (!(selectedWeapon && selectedBackpack))
+                        {
+                            list.AddRange(Constants.Weapons);
+                            list.AddRange(Constants.WeaponsWithBackpack);
+                            list.AddRange(Constants.Backpacks);
+                        }
+                        else if (!selectedWeapon)
+                        {
+                            list.AddRange(Constants.Weapons);
+                        }
+                        else if (!selectedBackpack)
+                        {
+                            list.AddRange(Constants.Backpacks);
+                        }
+                    }
+                }
+
+                stratagems.Add(item);
+
+                ++i;
+            }
+
+            stratagems.Sort(StratSort);
+
+            for (i = 0; i < stratagems.Count; ++i)
+            {
+                Console.WriteLine($"S {i + 1}:  {stratagems[i]}");
+            }
+
+            Console.WriteLine();
+        }
+
+        private static bool IsWeapon(string name, out bool hasPack)
+        {
+            hasPack = false;
+
+            return
+                Constants.Weapons.Contains(name)
+                || (hasPack = Constants.WeaponsWithBackpack.Contains(name));
+        }
+
+        private static bool IsBackpack(string name, out bool isWeapon)
+        {
+            isWeapon = false;
+
+            return
+                Constants.Backpacks.Contains(name)
+                || (isWeapon = Constants.WeaponsWithBackpack.Contains(name));
+        }
+
+        private static bool IsDefensive(string name)
+        {
+            return Constants.Defensives.Contains(name);
+        }
+
+        private static int StratSort(string s1, string s2)
+        {
+            if (s1.Contains("(Weapon")) return -1;
+            if (s2.Contains("(Weapon")) return 1;
+            if (s1.Contains("(Backpack)")) return -1;
+            if (s2.Contains("(Backpack)")) return 1;
+            if (s1.Contains("(Offense)")) return -1;
+            if (s2.Contains("(Offense)")) return 1;
+
+            return 0;
+        }
+    }
+}
